b59e45aa6b832b732a355fa1ac4d529dc9e169a2
samar <samargharbi877@gmail.com>
2024-04-29 22:20:38 +0200

    push

---
+++ js.js
@@ -0,0 +1,16 @@
+function fn (){
+    document.getElementById("expld").textContent="<h1>nouveau text</h1>" ;
+}
+function fn2(){
+    document.getElementById("expld").classList.add("d") ;
+}
+function fn3(){
+    document.getElementById("expld").classList.remove("d") ;
+}
+function fn4() {
+    let fruits = ["Apple", "Banana", "Orange"];
+    console.table(fruits);
+    
+   
+}
+
